a511f593048043aa69fbfd135bbeda591faabff7
sokol <sokol@192.168.1.3>
2017-09-15 10:43:17 +1200

    added 2 more classes

--- samplewebapp/src/main/java/dbAccess/CarCheck.java
+++ samplewebapp/src/main/java/dbAccess/CarCheck.java
@@ -2,7 +2,6 @@ package dbAccess;
 
 import java.io.IOException;
 import java.sql.ResultSet;
-//import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,8 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CarCheck extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	//private static final Object ParkingTimeFinish = null;
 	
 	private DBConnect newConnection;
        
---
+++ samplewebapp/src/main/java/dbAccess/Park.java
@@ -0,0 +1,19 @@
+package dbAccess;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Park {
+
+	public static void main(String[] args) {
+	
+		System.out.println("test");
+	}
+
+}
